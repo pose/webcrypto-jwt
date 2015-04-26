5d5ce2b4bcaeba5afffbd7d441cc63c1c6d2ac10
Alberto Pose <albertopose@gmail.com>
2015-04-26 20:20:46 -0300

    Removing unused functions.

--- index.js
+++ index.js
@@ -29,38 +29,6 @@
       return new Uint8Array(chars);
   }
 
-  function hexStringToUint8Array(hexString) {
-      if (hexString.length % 2 !== 0)
-          throw "Invalid hexString";
-      var arrayBuffer = new Uint8Array(hexString.length / 2);
-      for (var i = 0; i < hexString.length; i += 2) {
-          var byteValue = parseInt(hexString.substr(i, 2), 16);
-          if (isNaN(byteValue)) {
-            throw "Invalid hexString";
-          }
-          arrayBuffer[i/2] = byteValue;
-      }
-      return arrayBuffer;
-  }
-
-  function bytesToHexString(bytes) {
-      if (!bytes)
-          return null;
-      bytes = new Uint8Array(bytes);
-      var hexBytes = [];
-      for (var i = 0; i < bytes.length; ++i) {
-          var byteString = bytes[i].toString(16);
-          if (byteString.length < 2)
-              byteString = "0" + byteString;
-          hexBytes.push(byteString);
-      }
-      return hexBytes.join("");
-  }
-
-  function bytesToASCIIString(bytes) {
-      return String.fromCharCode.apply(null, new Uint8Array(bytes));
-  }
-
   function isString(s) {
     return typeof s === 'string';
   }
